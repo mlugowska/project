9d060c7fa9c7a756cd868381eef8274db72ab814
atwa <julian.machines@gmail.com>
2017-06-05 03:04:53 +0200

    heapsort nie podłaczone do API

--- restapp/serializers.py
+++ restapp/serializers.py
@@ -5,6 +5,7 @@ from restapp.models import Profile
 
 class ProfileSerializer(serializers.ModelSerializer):
 
+
     class Meta: # anything that’s not a field
         model = Profile # model name created to store profile
         fields = ('url', 'id', 'name', 'carrots')
--- restapp/views.py
+++ restapp/views.py
@@ -13,51 +13,55 @@ class ProfileViewSet(viewsets.ModelViewSet):
     """
 
     #  API endpoint that allows users to be viewed or edited
-
-    def heapSort(data):
-        dataSize = len(data)
-
-        def swap(parent_idx, child_idx):
-            if data[parent_idx] < data[child_idx]:
-                data[parent_idx], data[child_idx] = data[child_idx], data[parent_idx]
-
-        def indexing(parent_idx, unsortedData):
-
-            greaterElement_idx = lambda a, b: a if data[a] > data[b] else b
-
-            while parent_idx * 2 + 2 < unsortedData:
-                childLeft_idx = parent_idx * 2 + 1
-                childRight_idx = parent_idx * 2 + 2
-                greaterChild_idx = greaterElement_idx(childLeft_idx, childRight_idx)
-                swap(parent_idx, greaterChild_idx)
-                parent_idx = greaterChild_idx
-
-        def convert_to_heap():
-            for element in range((dataSize / 2) - 1, -1, -1):
-                indexing(element, dataSize)
-
-        def sorting():
-            iter = dataSize
-            while iter > 0:
-                iter -= 1
-            swap(iter, 0)
-            indexing(0, iter)
-
-        convert_to_heap()
-        sorting()
-
     queryset = Profile.objects.all()
-    # heapSort()
     serializer_class = ProfileSerializer
-
 
     # decorator can be used to add any custom endpoints that don't fit into the standard create/update/delete style.
     @detail_route()
     def highlight(self, *args, **kwargs):
         self.profile = self.get_object()
         return Response(self.profile.highlighted)
+    #
+    # @api_view(['GET'])
+    # def profile_get(request):
+    #     def heapSort(data):
+    #         dataSize = len(data)
+    #
+    #         def swap(parent_idx, child_idx):
+    #             if data[parent_idx] < data[child_idx]:
+    #                 data[parent_idx], data[child_idx] = data[child_idx], data[parent_idx]
+    #
+    #         def indexing(parent_idx, unsortedData):
+    #
+    #             greaterElement_idx = lambda a, b: a if data[a] > data[b] else b
+    #
+    #             while parent_idx * 2 + 2 < unsortedData:
+    #                 childLeft_idx = parent_idx * 2 + 1
+    #                 childRight_idx = parent_idx * 2 + 2
+    #                 greaterChild_idx = greaterElement_idx(childLeft_idx, childRight_idx)
+    #                 swap(parent_idx, greaterChild_idx)
+    #                 parent_idx = greaterChild_idx
+    #
+    #         def convert_to_heap():
+    #             for element in range((dataSize / 2) - 1, -1, -1):
+    #                 indexing(element, dataSize)
+    #
+    #         def sorting():
+    #             iter = dataSize
+    #             while iter > 0:
+    #                 iter -= 1
+    #             swap(iter, 0)
+    #             indexing(0, iter)
+    #
+    #         convert_to_heap()
+    #         sorting()
+    #
+    #     profile = Profile.objects.all()
+    #     carrot_field = Profile.objects.values_list('carrots', flat=True).order_by('carrots')
+    #     heapSort(carrot_field)
+    #     serializer = ProfileSerializer(carrot_field, many=True)
+    #     return Response(serializer.data)
 
-    def perform_create(self, serializer):
-        serializer.save()
+
 
 
